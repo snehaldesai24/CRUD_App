4ecd99e311b938ca5184796f9838f851cf1033f9
snehaldesai24 <72343205+snehaldesai24@users.noreply.github.com>
2025-02-12 19:05:27 +0530

    EditUser.jsx

--- Frontend/src/components/pages/EditUser.jsx
+++ Frontend/src/components/pages/EditUser.jsx
@@ -89,7 +89,7 @@ function EditUser() {
 
     return (
         <>
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="container flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto">
                     <form onSubmit={handleSubmitForm}>
                         <div className="space-y-12">
